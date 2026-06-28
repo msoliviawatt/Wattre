e3b4f177a93d38faf380ef40011929e48ec7b9fe
Olivia Watt <msoliviawatt@gmail.com>
2026-06-28 13:06:39 -0400

    oo, button make ellipse appear

--- wattre/Form1.vb
+++ wattre/Form1.vb
@@ -1,13 +1,26 @@
 ﻿Public Class Form1
+    Private currentRipple As Ripple
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'MessageBox.Show("Hola")
+
+        currentRipple = New Ripple()
+        currentRipple.Center = New Point(ClientSize.Width \ 2, ClientSize.Height \ 2)
+        currentRipple.Radius = 100
+
+        Invalidate() 'triggers repaint'
 
     End Sub
 
     Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
-        Dim diameter As Integer = 100
-        Dim x As Integer = (ClientSize.Width - diameter) / 2
-        Dim y As Integer = (ClientSize.Height - diameter) / 2
+
+        If (currentRipple Is Nothing) Then
+            Return
+        End If
+
+        Dim diameter As Integer = currentRipple.Radius * 2
+        Dim x As Integer = currentRipple.Center.X - currentRipple.Radius
+        Dim y As Integer = currentRipple.Center.Y - currentRipple.Radius
 
         'x, y, width, height'
         'e.Graphics.DrawEllipse(Pens.Blue, 100, 100, 200, 200)
@@ -15,6 +28,11 @@
     End Sub
 
     Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+
+        If (currentRipple IsNot Nothing) Then
+            currentRipple.Center = New Point(ClientSize.Width \ 2, ClientSize.Height \ 2)
+        End If
+
         Invalidate()
     End Sub
 End Class
---
+++ wattre/Ripple.vb
@@ -0,0 +1,6 @@
+﻿Imports System.Drawing
+
+Public Class Ripple
+    Public Property Center As Point
+    Public Property Radius As Integer
+End Class
