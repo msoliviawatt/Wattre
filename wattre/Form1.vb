afc442f42bd76092ba83e6e2224eef5ca06543ec
Olivia Watt <msoliviawatt@gmail.com>
2026-06-28 13:28:42 -0400

    wrapped in simulation object

--- wattre/Form1.vb
+++ wattre/Form1.vb
@@ -1,17 +1,21 @@
 ﻿Public Class Form1
     'Private currentRipple As Ripple
-    Private ripples = New List(Of Ripple) 'this should be like a c++ vector'
+    'Private ripples = New List(Of Ripple) 'this should be like a c++ vector'
+
+    Private simulation As New Simulation()
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'MessageBox.Show("Hola")
 
-        Dim currentRipple As New Ripple()
+        simulation.AddRipple(New Point(ClientSize.Width \ 2, ClientSize.Height \ 2), 50)
 
-        currentRipple = New Ripple()
-        currentRipple.Center = New Point(ClientSize.Width \ 2, ClientSize.Height \ 2)
-        currentRipple.Radius = 50
+        'Dim currentRipple As New Ripple()
 
-        ripples.Add(currentRipple)
+        'currentRipple = New Ripple()
+        'currentRipple.Center = New Point(ClientSize.Width \ 2, ClientSize.Height \ 2)
+        'currentRipple.Radius = 50
+
+        'ripples.Add(currentRipple)
 
         Invalidate() 'triggers repaint'
 
@@ -19,27 +23,29 @@
 
     Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
 
-        For Each currentRipple As Ripple In ripples
+        'For Each currentRipple As Ripple In ripples
 
-            Dim diameter As Integer = currentRipple.Radius * 2
-            Dim x As Integer = currentRipple.Center.X - currentRipple.Radius
-            Dim y As Integer = currentRipple.Center.Y - currentRipple.Radius
+        '    Dim diameter As Integer = currentRipple.Radius * 2
+        '    Dim x As Integer = currentRipple.Center.X - currentRipple.Radius
+        '    Dim y As Integer = currentRipple.Center.Y - currentRipple.Radius
 
-            'x, y, width, height'
+        '    'x, y, width, height'
+        '    e.Graphics.DrawEllipse(Pens.BlueViolet, x, y, diameter, diameter)
+
+        'Next
+
+        For Each ripple As Ripple In simulation.Ripples
+            Dim diameter As Integer = ripple.Radius * 2
+            Dim x As Integer = ripple.Center.X - ripple.Radius
+            Dim y As Integer = ripple.Center.Y - ripple.Radius
+
             e.Graphics.DrawEllipse(Pens.BlueViolet, x, y, diameter, diameter)
-
         Next
     End Sub
 
     Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
 
-        'If (currentRipple IsNot Nothing) Then
-        '    currentRipple.Center = New Point(ClientSize.Width \ 2, ClientSize.Height \ 2)
-        'End If
-
-        For Each currentRipple As Ripple In ripples
-            currentRipple.Center = New Point(ClientSize.Width \ 2, ClientSize.Height \ 2)
-        Next
+        simulation.MoveRipplesTo(New Point(ClientSize.Width \ 2, ClientSize.Height \ 2))
 
         Invalidate()
     End Sub
---
+++ wattre/Simulation.vb
@@ -0,0 +1,18 @@
+﻿'Imports System.Drawing
+
+Public Class Simulation
+    Public Property Ripples As New List(Of Ripple)
+
+    Public Sub AddRipple(center As Point, radius As Integer)
+        Dim newRipple As New Ripple()
+        newRipple.Center = center
+        newRipple.Radius = radius
+        Ripples.Add(newRipple)
+    End Sub
+
+    Public Sub MoveRipplesTo(center As Point)
+        For Each ripple As Ripple In Ripples
+            ripple.Center = center
+        Next
+    End Sub
+End Class
